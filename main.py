606aa4d67011574fbc0743d7cffef2bde426bd40
Berker Acır <30311105+berkeracir@users.noreply.github.com>
2022-11-02 20:48:37 +0000

    implementation of getting detailed course information

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pandas as pd
 from os.path import exists
 
-from scraping import scrape_all_courses, ALL_COURSES_FILE
-
+import scraping
 
 if __name__ == "__main__":
-	if not exists(ALL_COURSES_FILE):
-		all_courses = scrape_all_courses()
-		pd.DataFrame(data=all_courses).to_csv(ALL_COURSES_FILE,
-	                                      index=False,
-	                                      header=True)
+	if not exists(scraping.ALL_COURSES_FILE):
+		all_courses = scraping.scrape_all_courses_concurrently()
+		pd.DataFrame(data=all_courses).to_csv(scraping.ALL_COURSES_FILE,
+		                                      index=False,
+		                                      header=True)
 	else:
-		# TODO: If the file is updated long ago, update all courses.
-		pass
+		# TODO: If the file is updated long ago, it may be better to update it together with all courses' information.
+		all_courses = pd.read_csv(scraping.ALL_COURSES_FILE).to_dict()
+
+	if len(all_courses['name']) != len(all_courses['url']):
+		raise Exception("Incorrect Data of Courses")
+
+	#if not exists(scraping.COURSES_FILE):
+	for i in range(len(all_courses['name'])):
+		url = all_courses['url'][i]
+		scraping.scrape_course_information(url)
 
 #keep_alive()
 
--- scraping.py
+++ scraping.py
@@ -2,15 +2,19 @@ from bs4 import BeautifulSoup
 import requests  # TODO: use/try selenium webdriver
 import re
 from time import sleep, time
-
-import sys
+from multiprocessing import Pool
+import numpy as np
 
 COURSERA_URL = "https://www.coursera.org"
 COURSES_URL = "https://www.coursera.org/directory/courses"
 COURSES_URL_WITH_PAGE = "https://www.coursera.org/directory/courses?index=prod_all_products_term_optimization&page="
 
 ALL_COURSES_FILE = "all_courses.csv"
-SLEEP_TIME = 0.0
+CATEGORIES_FILE = "categories.csv"
+POOL_SIZE = 10
+SLEEP_TIME = 0.0  # TODO
+
+INSTRUCTOR_DIVIDER = " | "
 
 
 # Returns the latest page number in "https://www.coursera.org/directory/courses" or "https://www.coursera.org/directory/courses?page=100"
@@ -41,7 +45,7 @@ def get_courses_in_page(html_soup: BeautifulSoup) -> dict:
 	return courses_dict
 
 
-# Scrape all courses from Courseare together with their names and URLs
+# Scrapes all courses from Courseare together with their names and URLs
 def scrape_all_courses():
 	start = time()
 	response = requests.get(COURSES_URL)
@@ -56,14 +60,147 @@ def scrape_all_courses():
 			url = f"{COURSES_URL_WITH_PAGE}{page}"
 			response = requests.get(url)
 			html_soup = BeautifulSoup(response.content, 'html.parser')
-			
 
 		courses_in_page = get_courses_in_page(html_soup)
 		all_courses['name'].extend(courses_in_page['name'])
 		all_courses['url'].extend(courses_in_page['url'])
 		sleep(SLEEP_TIME)
-		
+
 	end = time()
-	print(f"\n{latest_page} pages and {len(all_courses['name'])} courses are scraped in {'{:.2f}'.format(end-start)} seconds.")
+	print(
+	 f"\n{latest_page} pages and {len(all_courses['name'])} courses are scraped in {'{:.2f}'.format(end-start)} seconds."
+	)
 
 	return all_courses
+
+
+# Scrapes course information (name, instructor(s), description, # of students, # of ratings, rating) from given URL
+
+
+# Returns a dictionary of courses {'names': [], 'urls': []}
+def get_courses_in_page_concurrently(page) -> dict:
+	print(f"Page: {page}")
+	url = f"{COURSES_URL_WITH_PAGE}{page}"
+	response = requests.get(url)
+	html_soup = BeautifulSoup(response.content, 'html.parser')
+	courses_dict = {'name': [], 'url': []}
+	course_links_columns = html_soup.find_all('div',
+	                                          {'data-testid': 'linksColumn'})
+
+	for course_links_column in course_links_columns:
+		course_links = course_links_column.find_all('a', href=True)
+		for course_link in course_links:
+			courses_dict['name'].append(course_link.get_text())
+			courses_dict['url'].append(COURSERA_URL + course_link['href'])
+
+	return courses_dict
+
+
+# Concurrently scrapes all courses from Coursera together with their names and URLs
+def scrape_all_courses_concurrently():
+	start = time()
+	response = requests.get(COURSES_URL)
+	html_soup = BeautifulSoup(response.content, 'html.parser')
+	latest_page = get_latest_page(html_soup)
+
+	all_courses = {'name': [], 'url': []}
+	choke = np.arange(1, latest_page + 1, POOL_SIZE)
+	for i in choke:
+		pool = Pool(POOL_SIZE)
+		data = pool.map(get_courses_in_page_concurrently,
+		                range(i, min(i + POOL_SIZE, latest_page + 1)))
+		pool.terminate()
+		pool.join()
+
+		for d in data:
+			all_courses['name'].extend(d['name'])
+			all_courses['url'].extend(d['url'])
+
+	end = time()
+	print(
+	 f"\n{latest_page} pages and {len(all_courses['name'])} courses are scraped in {'{:.2f}'.format(end-start)} seconds."
+	)
+
+	return all_courses
+
+
+def get_course_name(html_soup: BeautifulSoup) -> str:
+	banner_title = html_soup.find('h1', {'class': 'banner-title'})
+
+	if banner_title:
+		name = banner_title.get_text()
+	else:
+		programMiniModalName = html_soup.find('h1', {'id': 'programMiniModalName'})
+		name = programMiniModalName.get_text()
+
+	print(f"get_course_name: {name}")
+	return name
+
+
+def get_course_category(html_soup: BeautifulSoup) -> (str, str):
+	navigation_categories = html_soup.find(
+	 'div', {
+	  'role': 'navigation',
+	  'aria-label': re.compile('breadcrumbs|Breadcrumb')
+	 }).find_all('a', href=True)
+
+	category = navigation_categories[1].get_text()
+	subcategory = navigation_categories[2].get_text()
+
+	print(f"get_course_category: {category} > {subcategory}")
+	return (category, subcategory)
+
+
+def get_course_instructors(html_soup: BeautifulSoup) -> str:
+	instructor_avatars = html_soup.find_all(
+	 'a', {'data-click-key': re.compile('instructor_avatar')})
+
+	names = []
+
+	for instructor_avatar in instructor_avatars:
+		instructor_name = instructor_avatar.find(
+		 'h3', {'class': re.compile('instructor-name')})
+
+		if instructor_name:
+			names.append(instructor_name.get_text())
+		else:
+			instructor_name = instructor_avatar.find(
+			 'h2', {'class': re.compile('cds-119|css-10vfk66|cds-121')})
+			names.append(instructor_name.get_text())
+
+	names = INSTRUCTOR_DIVIDER.join(names)
+	print(f"get_course_instructors: {names}")
+
+	return names
+
+
+def get_course_description(html_soup: BeautifulSoup) -> str:
+	pass
+
+
+def get_course_enrollment(html_soup: BeautifulSoup) -> int:
+	pass
+
+
+def get_course_raters(html_soup: BeautifulSoup) -> int:
+	pass
+
+
+def get_course_rating(html_soup: BeautifulSoup) -> float:
+	pass
+
+
+def scrape_course_information(url):
+	response = requests.get(url)
+	html_soup = BeautifulSoup(response.content, 'html.parser')
+
+	try:
+		print(url)
+		get_course_name(html_soup)
+		get_course_category(html_soup)
+		get_course_instructors(html_soup)
+		print()
+	except AttributeError:
+		print(f"Skipping the broken link: {url}")
+		print()
+		return None
