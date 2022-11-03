3feaf45f064db86c912a3ccc6b176322997d4071
Berker Acır <30311105+berkeracir@users.noreply.github.com>
2022-11-03 15:20:15 +0000

    finalize scraping

--- main.py
+++ main.py
@@ -1,25 +1,64 @@
 import pandas as pd
 from os.path import exists
+import pickle
 
 import scraping
 
+ALL_COURSES_FILE = "all_courses.csv"
+ALL_COURSES_INFORMATION_FILE = "all_courses_information.csv"
+CATEGORIES_FILE = "categories.pkl"
+
 if __name__ == "__main__":
-	if not exists(scraping.ALL_COURSES_FILE):
+	update = False
+
+	if not exists(ALL_COURSES_FILE) or update:
+		update = True
 		all_courses = scraping.scrape_all_courses_concurrently()
-		pd.DataFrame(data=all_courses).to_csv(scraping.ALL_COURSES_FILE,
+		pd.DataFrame(data=all_courses).to_csv(ALL_COURSES_FILE,
 		                                      index=False,
 		                                      header=True)
 	else:
-		# TODO: If the file is updated long ago, it may be better to update it together with all courses' information.
-		all_courses = pd.read_csv(scraping.ALL_COURSES_FILE).to_dict()
+		# TODO: If the file is updated long ago, it may be better to update course pages.
+		all_courses = pd.read_csv(ALL_COURSES_FILE).to_dict()
 
 	if len(all_courses['name']) != len(all_courses['url']):
-		raise Exception("Incorrect Data of Courses")
+		raise Exception("Incorrect Courses Data")
 
-	#if not exists(scraping.COURSES_FILE):
-	for i in range(len(all_courses['name'])):
-		url = all_courses['url'][i]
-		scraping.scrape_course_information(url)
+	if not exists(ALL_COURSES_INFORMATION_FILE) or update:
+		update = True
+		all_course_information = scraping.scrape_course_information_concurrently(
+		 all_courses)
+		pd.DataFrame(data=all_course_information).to_csv(
+		 ALL_COURSES_INFORMATION_FILE, index=False, header=True)
+	else:
+		# TODO: If the file is updated long ago, it may be better to update course information.
+		all_course_information = pd.read_csv(ALL_COURSES_INFORMATION_FILE).to_dict()
+
+	if all(
+	  len(column) == len(all_course_information['name'])
+	  for column in all_course_information):
+		raise Exception("Incorrect Course Information Data")
+
+	if not exists(CATEGORIES_FILE) or update:
+		update = True
+		categories = {}
+
+		for i in range(len(all_course_information['category'])):
+			category = all_course_information['category'][i]
+			subcategory = all_course_information['subcategory'][i]
+
+			if category and subcategory:
+				if category in categories:
+					categories[category].add(subcategory)
+				else:
+					categories[category] = {subcategory}
+
+		with open(CATEGORIES_FILE, 'wb') as f:
+			pickle.dump(categories, f)
+	else:
+		# TODO: If the file is updated long ago, it may be better to update categories.
+		with open(CATEGORIES_FILE, 'rb') as f:
+			categories = pickle.load(f)
 
 #keep_alive()
 
--- scraping.py
+++ scraping.py
@@ -1,20 +1,21 @@
 from bs4 import BeautifulSoup
-import requests  # TODO: use/try selenium webdriver
-import re
-from time import sleep, time
 from multiprocessing import Pool
 import numpy as np
+import re
+import requests  # TODO: use/try selenium webdriver
+from time import time  #sleep
+import traceback
 
 COURSERA_URL = "https://www.coursera.org"
 COURSES_URL = "https://www.coursera.org/directory/courses"
 COURSES_URL_WITH_PAGE = "https://www.coursera.org/directory/courses?index=prod_all_products_term_optimization&page="
 
-ALL_COURSES_FILE = "all_courses.csv"
-CATEGORIES_FILE = "categories.csv"
-POOL_SIZE = 10
+POOL_SIZE_FOR_PAGE_SRAPING = 10
+POOL_SIZE_FOR_COURSE_SRAPING = 50
 SLEEP_TIME = 0.0  # TODO
 
-INSTRUCTOR_DIVIDER = " | "
+INSTRUCTOR_DIVIDER = ", "
+DESCRIPTION_CONNECTOR = "\n\n"
 
 
 # Returns the latest page number in "https://www.coursera.org/directory/courses" or "https://www.coursera.org/directory/courses?page=100"
@@ -64,7 +65,6 @@ def scrape_all_courses():
 		courses_in_page = get_courses_in_page(html_soup)
 		all_courses['name'].extend(courses_in_page['name'])
 		all_courses['url'].extend(courses_in_page['url'])
-		sleep(SLEEP_TIME)
 
 	end = time()
 	print(
@@ -74,12 +74,8 @@ def scrape_all_courses():
 	return all_courses
 
 
-# Scrapes course information (name, instructor(s), description, # of students, # of ratings, rating) from given URL
-
-
 # Returns a dictionary of courses {'names': [], 'urls': []}
 def get_courses_in_page_concurrently(page) -> dict:
-	print(f"Page: {page}")
 	url = f"{COURSES_URL_WITH_PAGE}{page}"
 	response = requests.get(url)
 	html_soup = BeautifulSoup(response.content, 'html.parser')
@@ -104,11 +100,16 @@ def scrape_all_courses_concurrently():
 	latest_page = get_latest_page(html_soup)
 
 	all_courses = {'name': [], 'url': []}
-	choke = np.arange(1, latest_page + 1, POOL_SIZE)
+	choke = np.arange(1, latest_page + 1, POOL_SIZE_FOR_PAGE_SRAPING)
 	for i in choke:
-		pool = Pool(POOL_SIZE)
-		data = pool.map(get_courses_in_page_concurrently,
-		                range(i, min(i + POOL_SIZE, latest_page + 1)))
+		print(
+		 f"\rScraping course pages: [{i}-{min(i + POOL_SIZE_FOR_PAGE_SRAPING - 1, latest_page)}]/{latest_page}",
+		 end='',
+		 flush=True)
+		pool = Pool(POOL_SIZE_FOR_PAGE_SRAPING)
+		data = pool.map(
+		 get_courses_in_page_concurrently,
+		 range(i, min(i + POOL_SIZE_FOR_PAGE_SRAPING, latest_page + 1)))
 		pool.terminate()
 		pool.join()
 
@@ -118,89 +119,225 @@ def scrape_all_courses_concurrently():
 
 	end = time()
 	print(
-	 f"\n{latest_page} pages and {len(all_courses['name'])} courses are scraped in {'{:.2f}'.format(end-start)} seconds."
+	 f"\n{latest_page} pages and {len(all_courses['name'])} courses are scraped in {'{:.2f}'.format(end-start)} seconds.\n"
 	)
 
 	return all_courses
 
 
+# Returns the course name
 def get_course_name(html_soup: BeautifulSoup) -> str:
+	name = None
 	banner_title = html_soup.find('h1', {'class': 'banner-title'})
 
 	if banner_title:
-		name = banner_title.get_text()
+		name = banner_title.get_text().strip()
 	else:
 		programMiniModalName = html_soup.find('h1', {'id': 'programMiniModalName'})
-		name = programMiniModalName.get_text()
 
-	print(f"get_course_name: {name}")
+		if programMiniModalName:
+			name = programMiniModalName.get_text().strip()
+
+	#print(f"get_course_name: {name}")
 	return name
 
 
+# Returns the course categories: category and subcategory
 def get_course_category(html_soup: BeautifulSoup) -> (str, str):
-	navigation_categories = html_soup.find(
+	category = None
+	subcategory = None
+	breadcrumbs = html_soup.find(
 	 'div', {
 	  'role': 'navigation',
 	  'aria-label': re.compile('breadcrumbs|Breadcrumb')
-	 }).find_all('a', href=True)
+	 })
 
-	category = navigation_categories[1].get_text()
-	subcategory = navigation_categories[2].get_text()
+	if breadcrumbs:
+		categories = breadcrumbs.find_all('a', href=True)
 
-	print(f"get_course_category: {category} > {subcategory}")
+		if categories and len(categories) == 3:
+			category = categories[1].get_text()
+			subcategory = categories[2].get_text()
+
+	#print(f"get_course_category: {category} > {subcategory}")
 	return (category, subcategory)
 
 
+# Returns the course instructor(s)
 def get_course_instructors(html_soup: BeautifulSoup) -> str:
+	names = []
 	instructor_avatars = html_soup.find_all(
 	 'a', {'data-click-key': re.compile('instructor_avatar')})
-
-	names = []
 
 	for instructor_avatar in instructor_avatars:
 		instructor_name = instructor_avatar.find(
 		 'h3', {'class': re.compile('instructor-name')})
 
 		if instructor_name:
-			names.append(instructor_name.get_text())
+			names.append(instructor_name.find_all(text=True, recursive=False)[0])
 		else:
 			instructor_name = instructor_avatar.find(
-			 'h2', {'class': re.compile('cds-119|css-10vfk66|cds-121')})
-			names.append(instructor_name.get_text())
+			 'h2', {'class': re.compile('css-10vfk66')})
 
-	names = INSTRUCTOR_DIVIDER.join(names)
-	print(f"get_course_instructors: {names}")
+			if instructor_name:
+				names.append(instructor_name.get_text())
 
+	if not names:
+		instructor_avatar = html_soup.find(
+		 'a', {'data-click-key': re.compile('nav_item_instructors')})
+
+		if instructor_avatar:
+			span = instructor_avatar.find('span')
+
+			if span:
+				names.append(span.get_text())
+
+	names = INSTRUCTOR_DIVIDER.join(names) if names else None
+	#print(f"get_course_instructors: {names}")
 	return names
 
 
+# Returns the course description
 def get_course_description(html_soup: BeautifulSoup) -> str:
-	pass
+	description = None
+	about_section = html_soup.find('div', {'class': re.compile('about-section')})
+
+	if about_section:
+		paragraphs = about_section.find_all('p')
+
+		for paragraph in paragraphs:
+			text = paragraph.get_text()
+			description = text if not description else (description +
+			                                            DESCRIPTION_CONNECTOR + text)
+
+	else:
+		about_section = html_soup.find('p', {'class': re.compile('css-ngtbbz')})
+
+		if about_section:
+			description = about_section.get_text()
+
+	#print(f"get_course_description: {description}")
+	return description
 
 
-def get_course_enrollment(html_soup: BeautifulSoup) -> int:
-	pass
+# Returns the course metrics: the number of already enrolled students or recent views
+def get_course_metrics(html_soup: BeautifulSoup) -> (int, int):
+	enrollment = None
+	views = None
+	product_metrics = html_soup.find_all('div', {'class': 'rc-ProductMetrics'})
+
+	for product_metric in product_metrics:
+		span = product_metric.find('span')
+
+		if span:
+			text = span.get_text()
+			number = int(
+			 re.findall(r'[\d]+[.,\d]+', text)[0].replace(',', '').replace('.', ''))
+
+			if 'views' in text:
+				views = number
+			elif 'enroll' in text:
+				enrollment = number
+
+	#print(f"get_course_metrics: Enrolled-{enrollment}, Views-{views}")
+	return (enrollment, views)
 
 
-def get_course_raters(html_soup: BeautifulSoup) -> int:
-	pass
+# Returns the course review summary: rating and ratings count
+def get_course_reviews(html_soup: BeautifulSoup) -> int:
+	rating = None
+	ratings_count = None
+
+	span_rating_text = html_soup.find(
+	 'span', {'class': re.compile('rating-text number-rating')})
+	if span_rating_text:
+		rating = float(span_rating_text.find_all(text=True, recursive=False)[0])
+
+	span_ratings_count = html_soup.find(
+	 'span', {'data-test': re.compile('ratings-count')})
+	if not span_ratings_count:
+		span_ratings_count = html_soup.find(
+		 'p', {'data-test': re.compile('ratings-count')})
+	if span_ratings_count:
+		ratings_count = int(
+		 re.findall(r'[\d]+[.,\d]+',
+		            span_ratings_count.get_text())[0].replace('.',
+		                                                      '').replace(',', ''))
+
+	#print(f"get_course_reviews: Rating-{rating}, Ratings Count-{ratings_count}")
+	return (rating, ratings_count)
 
 
-def get_course_rating(html_soup: BeautifulSoup) -> float:
-	pass
-
-
-def scrape_course_information(url):
+# Scrapes course information (name, url, category, subcategory, instructor(s), description, # of enrolled students, # of recent views, rating, # of ratings) from given URL
+def scrape_course_information(name, url):
 	response = requests.get(url)
 	html_soup = BeautifulSoup(response.content, 'html.parser')
 
 	try:
-		print(url)
-		get_course_name(html_soup)
-		get_course_category(html_soup)
-		get_course_instructors(html_soup)
-		print()
-	except AttributeError:
-		print(f"Skipping the broken link: {url}")
-		print()
+		course_name = get_course_name(html_soup)
+		course_name = course_name if course_name else name
+		(category, subcategory) = get_course_category(html_soup)
+		instructors = get_course_instructors(html_soup)
+		description = get_course_description(html_soup)
+		(enrollment, views) = get_course_metrics(html_soup)
+		(rating, raters) = get_course_reviews(html_soup)
+		return (course_name, url, category, subcategory, instructors, description,
+		        enrollment, views, rating, raters)
+	except:
+		print(f"\nSkipping the broken link: {url}")
+		traceback.print_exc()
 		return None
+
+
+# Concurrently scrapes all course information from URL
+def scrape_course_information_concurrently(all_courses: dict) -> dict:
+	start = time()
+	print("Scraping all course information...")
+	all_course_information = {
+	 'name': [],
+	 'url': [],
+	 'category': [],
+	 'subcategory': [],
+	 'instructors': [],
+	 'description': [],
+	 'enrollment': [],
+	 'views': [],
+	 'rating': [],
+	 'raters': []
+	}
+	choke = np.arange(0, len(all_courses['name']), POOL_SIZE_FOR_COURSE_SRAPING)
+	for i in choke:
+		print(
+		 f"\rScraping course information: [{i+1}-{min(i + POOL_SIZE_FOR_COURSE_SRAPING, len(all_courses['name']))}]/{len(all_courses['name'])}",
+		 end='',
+		 flush=True)
+		pool = Pool(POOL_SIZE_FOR_COURSE_SRAPING)
+		arguments = [(all_courses['name'][j], all_courses['url'][j]) for j in range(
+		 i, min(i + POOL_SIZE_FOR_COURSE_SRAPING, len(all_courses['name'])))]
+		data = pool.starmap(scrape_course_information, arguments)
+		pool.terminate()
+		pool.join()
+
+		for d in data:
+			if d is None:
+				continue
+
+			(name, url, category, subcategory, instructors, description, enrollment,
+			 views, rating, raters) = d
+			all_course_information['name'].append(name)
+			all_course_information['url'].append(url)
+			all_course_information['category'].append(category)
+			all_course_information['subcategory'].append(subcategory)
+			all_course_information['instructors'].append(instructors)
+			all_course_information['description'].append(description)
+			all_course_information['enrollment'].append(enrollment)
+			all_course_information['views'].append(views)
+			all_course_information['rating'].append(rating)
+			all_course_information['raters'].append(raters)
+
+	end = time()
+	print(
+	 f"\n{len(all_course_information['name'])} courses are scraped in {'{:.2f}'.format(end-start)} seconds.\n"
+	)
+
+	return all_course_information
