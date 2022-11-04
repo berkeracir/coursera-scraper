80e347af62f75fa51418185affdb92fc8b0f1e6a
Berker Acır <30311105+berkeracir@users.noreply.github.com>
2022-11-04 08:54:16 +0000

    finalize the scraper

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pandas as pd
-from os.path import exists
+import os
 import pickle
+import math
+import time
 
 import scraping
+import server
 
 ALL_COURSES_FILE = "all_courses.csv"
 ALL_COURSES_INFORMATION_FILE = "all_courses_information.csv"
 CATEGORIES_FILE = "categories.pkl"
+CSV_FOLDER = "csv_files/"
 
 if __name__ == "__main__":
 	update = False
 
-	if not exists(ALL_COURSES_FILE) or update:
+	# scrape Coursera courses into csv file
+	if not os.path.exists(ALL_COURSES_FILE) or update:
 		update = True
 		all_courses = scraping.scrape_all_courses_concurrently()
 		pd.DataFrame(data=all_courses).to_csv(ALL_COURSES_FILE,
@@ -24,22 +29,27 @@ if __name__ == "__main__":
 	if len(all_courses['name']) != len(all_courses['url']):
 		raise Exception("Incorrect Courses Data")
 
-	if not exists(ALL_COURSES_INFORMATION_FILE) or update:
+	# scrape all Coursera course information into csv file
+	if not os.path.exists(ALL_COURSES_INFORMATION_FILE) or update:
 		update = True
 		all_course_information = scraping.scrape_course_information_concurrently(
 		 all_courses)
-		pd.DataFrame(data=all_course_information).to_csv(
-		 ALL_COURSES_INFORMATION_FILE, index=False, header=True)
+		df_all_course_information = pd.DataFrame(data=all_course_information)
+		df_all_course_information.to_csv(ALL_COURSES_INFORMATION_FILE,
+		                                 index=False,
+		                                 header=True)
 	else:
 		# TODO: If the file is updated long ago, it may be better to update course information.
-		all_course_information = pd.read_csv(ALL_COURSES_INFORMATION_FILE).to_dict()
+		df_all_course_information = pd.read_csv(ALL_COURSES_INFORMATION_FILE)
+		all_course_information = df_all_course_information.to_dict()
 
 	if all(
 	  len(column) == len(all_course_information['name'])
 	  for column in all_course_information):
 		raise Exception("Incorrect Course Information Data")
 
-	if not exists(CATEGORIES_FILE) or update:
+	# save category information
+	if not os.path.exists(CATEGORIES_FILE) or update:
 		update = True
 		categories = {}
 
@@ -47,11 +57,17 @@ if __name__ == "__main__":
 			category = all_course_information['category'][i]
 			subcategory = all_course_information['subcategory'][i]
 
-			if category and subcategory:
+			if category and subcategory and isinstance(category, str) and isinstance(
+			  subcategory, str):
 				if category in categories:
 					categories[category].add(subcategory)
 				else:
 					categories[category] = {subcategory}
+
+		sorted_categories = {}
+		for category in sorted(categories.keys()):
+			sorted_categories[category] = sorted(categories[category])
+		categories = sorted_categories
 
 		with open(CATEGORIES_FILE, 'wb') as f:
 			pickle.dump(categories, f)
@@ -60,8 +76,39 @@ if __name__ == "__main__":
 		with open(CATEGORIES_FILE, 'rb') as f:
 			categories = pickle.load(f)
 
-#keep_alive()
+	# create CSV_FOLDER if it doesn't exist
+	if not os.path.exists(CSV_FOLDER):
+		os.makedirs(CSV_FOLDER)
 
-#while True:
-#	print("Alive...")
-#	time.sleep(10)
+	# create sorted csv file for whole data
+	csv_filepath = os.path.join(CSV_FOLDER, "all_courses.csv")
+	if not os.path.exists(csv_filepath) or update:
+		df_sorted_all_course_information = df_all_course_information.sort_values(
+		 by=['name'])
+		df_sorted_all_course_information.to_csv(csv_filepath,
+		                                        index=False,
+		                                        header=True)
+	else:
+		df_sorted_all_course_information = pd.read_csv(csv_filepath)
+
+	# obtain category and subcategories while creating csv files
+	for category in categories:
+		df_category = df_sorted_all_course_information[(
+		 df_sorted_all_course_information['category'] == category)]
+		csv_filepath = os.path.join(CSV_FOLDER, f"{category}.csv")
+		if not os.path.exists(csv_filepath) or update:
+			df_category.to_csv(csv_filepath, index=False, header=True)
+
+		for subcategory in categories[category]:
+			df_category_subcategory = df_sorted_all_course_information[
+			 (df_sorted_all_course_information['category'] == category)
+			 & (df_sorted_all_course_information['subcategory'] == subcategory)]
+			csv_filepath = os.path.join(CSV_FOLDER, f"{category}-{subcategory}.csv")
+			if not os.path.exists(csv_filepath) or update:
+				df_category_subcategory.to_csv(csv_filepath, index=False, header=True)
+
+	server.start(categories)
+
+	while True:
+		print("Alive...")
+		time.sleep(10)
--- server.py
+++ server.py
@@ -1,15 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, abort
 from threading import Thread
 
-app = Flask(' ')
+app = Flask(__name__)
+
+CATEGORIES = None
+
 
 @app.route('/')
 def home():
-	return "Hello World"
+	global CATEGORIES
+	return render_template('index.html', categories=CATEGORIES)
+
+
+@app.route('/get-file', methods=['GET'])
+def get_file():
+	return redirect(
+	 url_for('get_csv', filename=request.args.get('category_subcategory')))
+
+
+@app.route('/csv/<filename>', methods=['GET'])
+def get_csv(filename):
+	try:
+		download_name = filename.lower().replace(' ', '_') + ".csv"
+		return send_from_directory("csv_files",
+		                           filename + ".csv",
+		                           as_attachment=False,
+		                           download_name=download_name)
+	except FileNotFoundError:
+		abort(404)
+
 
 def run():
-	app.run(host='0.0.0.0', port=8000)
+	app.run(host='0.0.0.0', port=8000, threaded=True)
 
-def keep_alive():
+
+def start(categories):
+	global CATEGORIES
+	CATEGORIES = categories
+
 	t = Thread(target=run)
 	t.start()
